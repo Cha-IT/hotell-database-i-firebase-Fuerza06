a97a8c8b3223cf29c26193fbf3bcc47dfe368306
Vladislav Zonca <142877484+Fuerza06@users.noreply.github.com>
2024-04-23 09:33:24 +0200

    Add files via upload

---
+++ Firebase oppgave/firebaseconfig.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAc6m8Dyt-uN9FPeSC95Jh2Ij-J5yS364k",
+    authDomain: "hotell-ef53e.firebaseapp.com",
+    projectId: "hotell-ef53e",
+    storageBucket: "hotell-ef53e.appspot.com",
+    messagingSenderId: "717970361276",
+    appId: "1:717970361276:web:259ad9525aac01e48cf9b9"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
